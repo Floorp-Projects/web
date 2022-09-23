ddbbbe4c50d36ce9f2e41c0a51a027a97772a123
Itta Funahashi <itta@itta.dev>
2022-09-24 08:25:59 +0900

    add features

---
+++ components/FeatureCard.jsx
@@ -0,0 +1,30 @@
+import { GridItem, HStack, Flex, Icon, Heading, Text } from '@chakra-ui/react';
+
+function FeatureCard({ title, icon, description }) {
+  return (
+    <GridItem>
+      <HStack spacing={5}>
+        <Flex
+          w={10}
+          h={10}
+          alignItems="center"
+          justifyContent="center"
+          borderColor="gray.300"
+          borderWidth={1}
+          bg="white"
+          borderRadius="md"
+        >
+          {icon}
+        </Flex>
+        <Heading as="h3" fontSize="md" fontWeight="medium">
+          {title}
+        </Heading>
+      </HStack>
+      <Text mt={5} color="gray.600">
+        {description}
+      </Text>
+    </GridItem>
+  );
+}
+
+export default FeatureCard;
