4915135e05b363beaaf8b389ea3cecf0e7214828
Itta Funahashi <itta@itta.dev>
2022-09-27 19:34:35 +0900

    fix font size

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -5,10 +5,10 @@ function FeatureCard({ title, description, buttonLabel }) {
     <Flex h="400px" alignItems="center" _odd={{ flexDirection: 'row-reverse' }}>
       <Box h="300px" w="420px" bg="gray.200" mx="auto" />
       <Box w="420px" mx="auto" px={4}>
-        <Heading as="h2" fontSize="2xl" fontWeight="bold">
+        <Heading as="h2" fontSize="xl" fontWeight="bold">
           {title}
         </Heading>
-        <Text color="gray.600" mt={7}>
+        <Text color="gray.600" fontSize="sm" lineHeight="" mt={7}>
           {description}
         </Text>
         <Button w="full" mt={7} variant="outline">
