72f8f1e13367d1b88ac25a99236ca6fbc87a8c4d
Itta Funahashi <itta@itta.dev>
2022-09-28 18:48:34 +0900

    fix feature

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -2,9 +2,20 @@ import { Box, Flex, Heading, Text, Button } from '@chakra-ui/react';
 
 function FeatureCard({ title, description, buttonLabel }) {
   return (
-    <Flex h="400px" alignItems="center" _odd={{ flexDirection: 'row-reverse' }}>
-      <Box w="440px" h="300px" bg="gray.200" mx="auto" />
-      <Box w="440px" mx="auto" px={4}>
+    <Flex
+      my={20}
+      alignItems="center"
+      flexDirection={{ base: 'column', sm: 'row' }}
+      _odd={{ flexDirection: { base: 'column', sm: 'row-reverse' } }}
+    >
+      <Box
+        w={{ base: 'full', sm: '440px' }}
+        my={{ base: 12, sm: 0 }}
+        h="300px"
+        bg="gray.200"
+        mx="auto"
+      />
+      <Box w={{ base: 'full', sm: '440px' }} mx="auto" px={4}>
         <Heading as="h2" fontSize="xl" fontWeight="bold">
           {title}
         </Heading>
