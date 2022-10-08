ff0e839d6cf5a8dd721eb76552d14e03ca538886
Itta Funahashi <itta@itta.dev>
2022-10-08 10:21:46 +0900

    Show image

--- components/Feature.jsx
+++ components/Feature.jsx
@@ -9,13 +9,9 @@ function FeatureCard({ title, image, description, buttonLabel }) {
       flexDirection={{ base: 'column', sm: 'row' }}
       _odd={{ flexDirection: { base: 'column', sm: 'row-reverse' } }}
     >
-      <Box
-        w={{ base: 'full', sm: '480px' }}
-        my={{ base: 12, sm: 0 }}
-        h="270px"
-        bg="gray.200"
-        mx="auto"
-      />
+      <Box w={{ base: 'full', sm: '480px' }} my={{ base: 12, sm: 0 }} h="270px" mx="auto">
+        <Image src={image} width="480px" height="270px" alt="Feature Image" />
+      </Box>
       <Box w={{ base: 'full', sm: '480px' }} mx="auto" px={4}>
         <Heading as="h2" fontSize="xl" fontWeight="bold">
           {title}
