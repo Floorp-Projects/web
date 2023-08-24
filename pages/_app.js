0dfd979b950155d82d818c7d2a4da2f8836e09ef
Itta Funahashi <itta@itta.dev>
2023-08-24 21:11:19 +0900

    feat: install next-i18next

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { appWithTranslation } from 'next-i18next';
+import nextI18NextConfig from '../next-i18next.config.js';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import './styles.css';
@@ -20,7 +22,10 @@ const theme = extendTheme({
 
 function App({ Component, pageProps }) {
   useEffect(() => {
-    console.log('%cつくったひと：http://itta.dev', 'font-size: 1.5em; color: #000;');
+    console.log(
+      '%cDeveloped by http://itta.dev',
+      'font-size: 1.5em; color: #000; font-style: italic; padding: 20px'
+    );
   }, []);
   return (
     <ChakraProvider theme={theme}>
@@ -29,4 +34,4 @@ function App({ Component, pageProps }) {
   );
 }
 
-export default App;
+export default appWithTranslation(App, nextI18NextConfig);
