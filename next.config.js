ec8789fad928980fbd62375ab741213eb6802c98
Itta Funahashi <itta@itta.dev>
2023-08-24 21:11:19 +0900

    feat: install next-i18next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withExportImages = require("next-export-optimize-images");
+const withExportImages = require('next-export-optimize-images');
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = withExportImages({
   reactStrictMode: true,
   swcMinify: true,
+  i18n,
 });
 
 module.exports = nextConfig;
