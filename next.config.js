f9c68f384cbe63da1860448a0ccea2c336a10aba
Itta Funahashi <itta@itta.dev>
2023-08-28 19:54:22 +0900

    feat: support ssg build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const withExportImages = require('next-export-optimize-images');
-const { i18n } = require('./next-i18next.config');
 
 const nextConfig = withExportImages({
   reactStrictMode: true,
   swcMinify: true,
-  i18n,
+  trailingSlash: true,
 });
 
 module.exports = nextConfig;
