b01727fb39bab50611fce035d3f583eef1363ceb
Itta Funahashi <itta@itta.dev>
2023-08-28 19:54:22 +0900

    feat: support ssg build

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
+  debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
