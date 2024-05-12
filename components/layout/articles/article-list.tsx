15581dd9f3fe4ba6aee85d14f824df77463ffecd
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 20:27:21 +0200

    added article components

---
+++ components/layout/articles/article-list.tsx
@@ -0,0 +1,36 @@
+import Article, {ArticleResponse} from "@/components/layout/articles/article";
+import React from "react";
+import Link from "next/link";
+import {cn} from "@/lib/utils";
+import {buttonVariants} from "@/components/ui/button";
+
+type ArticleListProps = {
+  articles: ArticleResponse[];
+  fadeOutAfter?: number;
+  seeMore?: {
+    text: string;
+    link: string;
+  }
+}
+
+export default function ArticleList({articles, fadeOutAfter, seeMore}: ArticleListProps) {
+  fadeOutAfter = fadeOutAfter || articles.length;
+  articles = articles.slice(0, fadeOutAfter);
+  const seeMoreContent = seeMore ? (
+    <Link
+      className={cn(
+        buttonVariants({variant: 'default'}),
+        "absolute bottom-16 left-1/2 transform -translate-x-1/2"
+      )}
+      target={"_blank"}
+      href={seeMore.link}>{seeMore.text}</Link>
+  ) : null;
+  return (
+    <div className={"relative"}>
+      {articles.map((article, index) => (
+        <Article key={index} {...article} isFadeOut={index === fadeOutAfter - 1} />
+      ))}
+      {seeMoreContent}
+    </div>
+  )
+}
