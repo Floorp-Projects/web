737905971fa6e141400ceaf92669bcf542d52a17
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 17:18:06 +0200

    language fixes

--- components/language-select.tsx
+++ components/language-select.tsx
@@ -1,16 +1,28 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import {usePathname} from 'next/navigation'
 import Link from 'next/link'
-import { i18n } from '@/i18n/i18n.config';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
-import { Button } from './ui/button';
+import {i18n} from '@/i18n/i18n.config';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from './ui/dropdown-menu';
+import {Button} from './ui/button';
+import {isNotStarted, isReviewed} from "@/i18n/validators";
 
-export interface Resources {
-  dropdownTitle: string;
+export interface LanguageSelectProps {
+  languageSelect: string;
+  inReview: string;
+  waitingForContributions: string;
 }
 
-export default function LanguageSelect({ res }: { res: Resources }) {
+export default function LanguageSelect({languageSelect, inReview, waitingForContributions}: LanguageSelectProps) {
 
   const pathName = usePathname()
   const redirectedPathName = (locale: string) => {
@@ -23,22 +35,38 @@ export default function LanguageSelect({ res }: { res: Resources }) {
   const dict = {
     en: "English",
     ja: "日本語",
+    ru: "Русский",
+    hu: "Magyar",
+  } as Record<string, string>;
+
+  const extendLocaleIfNecessary = (locale: string) => {
+    if (isNotStarted(locale)) {
+      return `${dict[locale]} (${waitingForContributions})`
+    }
+
+    if (!isReviewed(locale)) {
+      return `${dict[locale]} (${inReview})`
+    }
+
+    return dict[locale]
   }
 
-  return (
 
+  return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">{res.dropdownTitle}</Button>
+        <Button variant="outline">{languageSelect}</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>{res.dropdownTitle}</DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuLabel>{languageSelect}</DropdownMenuLabel>
+        <DropdownMenuSeparator/>
         {i18n.locales.map((locale) => {
           return (
-            <Link key={locale} href={redirectedPathName(locale)}>
-              <DropdownMenuItem >{dict[locale]}</DropdownMenuItem>
-            </Link>
+            <DropdownMenuItem key={locale} disabled={!isReviewed(locale)} asChild>
+              <Link href={redirectedPathName(locale)}>
+                {extendLocaleIfNecessary(locale)}
+              </Link>
+            </DropdownMenuItem>
           )
         })}
       </DropdownMenuContent>
--- i18n/dictionaries.ts
+++ i18n/dictionaries.ts
@@ -5,6 +5,8 @@ import type { Locale } from './i18n.config';
 const dictionaries = {
   en: () => import('@/dictionaries/en.json').then((module) => module.default),
   ja: () => import('@/dictionaries/ja.json').then((module) => module.default),
+  hu: () => import('@/dictionaries/hu.json').then((module) => module.default),
+  ru: () => import('@/dictionaries/ru.json').then((module) => module.default),
 }
 
 export const getDictionary = async (locale: Locale) =>
