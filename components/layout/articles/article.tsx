bc6a6a8c97049513cf37b07771c385fe7a122322
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 20:27:21 +0200

    added article components

---
+++ components/layout/articles/article.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
+import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
+import {CardContainer} from "@/components/ui/3d-card";
+import {cn} from "@/lib/utils";
+
+export type ArticleLocalizableProps = {
+  postedBy: string;
+  postedOn: string;
+  readMore: string;
+}
+
+export type ArticleResponse = {
+  title: string;
+  date: string;
+  author: string;
+  link: string;
+  description: string;
+  image: string;
+}
+
+type ArticleProps = {
+  locale?: ArticleLocalizableProps;
+  readMore?: string;
+  isFadeOut?: boolean;
+} & ArticleResponse;
+
+export default function Article(props: ArticleProps) {
+  const {title, date, author, link, description, image, locale, readMore, isFadeOut} = props;
+  const readMoreContent = readMore ? (
+    <Link href={readMore}
+          className={cn(
+            buttonVariants({variant: 'default'})
+          )}>{locale ? locale.readMore : "Read more"}</Link>
+  ) : <></>;
+
+  const fadeOutDiv = isFadeOut ? (
+    <div className="absolute bottom-0 left-0 w-full h-full bg-gradient-to-b from-transparent from-0% to-background to-60%"/>
+  ) : <></>;
+
+  return (
+    <div className={"relative my-4"}>
+      {fadeOutDiv}
+      <Link href={link} target={'_blank'}>
+        <Card>
+          <CardHeader className={'text-2xl font-bold'}>{title}</CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-3">
+              <div className="col-span-2">
+                {description}
+                {readMoreContent}
+              </div>
+              <div className={"flex justify-center items-center col-span-1"}>
+                <Image src={image} alt={title} width={200} height={200}/>
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className={"flex justify-between"}>
+            <p>{date}</p>
+            <p>{author}</p>
+          </CardFooter>
+        </Card>
+      </Link>
+    </div>
+  )
+}
