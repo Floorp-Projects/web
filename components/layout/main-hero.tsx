4c4cbc83523952bcb9312be905bc7b3080c753b1
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 08:38:03 +0200

    layout components

---
+++ components/layout/main-hero.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "../ui/hero-highlight";
+import {replaceComponent as rC} from "@/i18n/utils";
+
+type MainHeroProps = {
+  translation: string;
+};
+
+export function MainHero({translation}: MainHeroProps) {
+  const formatted = rC(translation, {key: "1", type: Highlight, rest: {className: "text-black dark:text-white"}});
+  return (
+    <HeroHighlight>
+      <motion.h1
+        initial={{
+          opacity: 0,
+          y: 20,
+        }}
+        animate={{
+          opacity: 1,
+          y: [20, -5, 0],
+        }}
+        transition={{
+          duration: 0.5,
+          ease: [0.4, 0.0, 0.2, 1],
+        }}
+        className="text-2xl px-4 md:text-4xl lg:text-7xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-left mx-auto "
+      >
+        {formatted}
+      </motion.h1>
+    </HeroHighlight>
+  );
+}
