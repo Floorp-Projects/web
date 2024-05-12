6f919407807931892a139b88c4a4dc388aa4ab2f
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 11:53:07 +0200

    added lang support to header link

--- components/layout/header-and-side-nav.tsx
+++ components/layout/header-and-side-nav.tsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
-import {Menu, Package2} from "lucide-react";
+import {Menu} from "lucide-react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import {Button, buttonVariants} from "@/components/ui/button";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
 import {ThemeSwitch} from "@/components/theme-switch";
 import {cn} from "@/lib/utils";
+import LanguageSelect from "@/components/language-select";
 
-export default function HeaderAndSideNav() {
+type HeaderAndSideNavProps = {
+  selectTitle: string;
+  lang: string;
+}
+
+export default function HeaderAndSideNav({selectTitle, lang}: HeaderAndSideNavProps) {
   return (
     <header className="top-0 z-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav
         className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href={`/${lang}`}
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <Image src={Logo} alt="logo" width={100} height={50} />
@@ -34,10 +40,10 @@ export default function HeaderAndSideNav() {
         <SheetContent side="left">
           <nav className="grid gap-6 text-lg font-medium">
             <Link
-              href="#"
+              href={`/${lang}`}
               className="flex items-center gap-2 text-lg font-semibold"
             >
-              <Package2 className="h-6 w-6"/>
+              <Image src={Logo} alt="logo" width={100} height={50} />
               <span className="sr-only">Acme Inc</span>
             </Link>
             <Link
@@ -53,8 +59,9 @@ export default function HeaderAndSideNav() {
         </SheetContent>
       </Sheet>
       <div className="flex w-full gap-4 justify-end md:gap-2 lg:gap-4">
+        <LanguageSelect res={{dropdownTitle: selectTitle}} />
         <ThemeSwitch />
-        <Link href={'/download'}
+        <Link href={`/${lang}/download`}
               className={cn(
                 buttonVariants({variant: "default"})
               )}
