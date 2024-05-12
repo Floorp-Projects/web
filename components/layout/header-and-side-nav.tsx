373bc78a83b725c39565980b0d9ca515c515434f
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 08:38:03 +0200

    layout components

---
+++ components/layout/header-and-side-nav.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import {Menu, Package2} from "lucide-react";
+import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import {Button, buttonVariants} from "@/components/ui/button";
+
+export default function HeaderAndSideNav() {
+  return (
+    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav
+        className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Package2 className="h-6 w-6"/>
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="shrink-0 md:hidden"
+          >
+            <Menu className="h-5 w-5"/>
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Package2 className="h-6 w-6"/>
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+            <Link
+              href="#"
+              className="text-muted-foreground hover:text-foreground"
+            >
+              Star us
+            </Link>
+            <Link href="#" className="hover:text-foreground">
+              Download
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <div className="flex w-full gap-4 justify-end md:gap-2 lg:gap-4">
+        <Link href={'/download'} className={buttonVariants({variant: "default"})}>Download</Link>
+      </div>
+    </header>
+  )
+}
---
+++ components/layout/hero-card.tsx
@@ -0,0 +1,63 @@
+import {CardBody, CardContainer, CardItem} from "@/components/ui/3d-card";
+import Image from "next/image";
+import hero from "@/public/hero.png";
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
+
+export type MainHeroCardProps = {
+  header: string;
+  description: string;
+  sourceCode: string;
+  download: string;
+}
+
+export default function MainHeroCard({header, description, sourceCode, download}: MainHeroCardProps) {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody
+        className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          {header}
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          {description}
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src={hero}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://github.com/Floorp-Projects/Floorp"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            {sourceCode}
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="/download"
+            className={buttonVariants({variant: "default"})}
+          >
+            {download}
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  )
+}
