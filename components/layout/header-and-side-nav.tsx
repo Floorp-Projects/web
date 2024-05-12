5d42c7dfd7091c52340a7cd46dd3da9764bf3118
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 11:30:01 +0200

    fixed responsivity errors

--- components/layout/header-and-side-nav.tsx
+++ components/layout/header-and-side-nav.tsx
@@ -2,17 +2,21 @@ import Link from "next/link";
 import {Menu, Package2} from "lucide-react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import {Button, buttonVariants} from "@/components/ui/button";
+import Image from "next/image";
+import Logo from "@/public/logo.svg";
+import {ThemeSwitch} from "@/components/theme-switch";
+import {cn} from "@/lib/utils";
 
 export default function HeaderAndSideNav() {
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="top-0 z-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav
         className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
           href="#"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
-          <Package2 className="h-6 w-6"/>
+          <Image src={Logo} alt="logo" width={100} height={50} />
           <span className="sr-only">Acme Inc</span>
         </Link>
       </nav>
@@ -49,7 +53,12 @@ export default function HeaderAndSideNav() {
         </SheetContent>
       </Sheet>
       <div className="flex w-full gap-4 justify-end md:gap-2 lg:gap-4">
-        <Link href={'/download'} className={buttonVariants({variant: "default"})}>Download</Link>
+        <ThemeSwitch />
+        <Link href={'/download'}
+              className={cn(
+                buttonVariants({variant: "default"})
+              )}
+        >Download</Link>
       </div>
     </header>
   )
