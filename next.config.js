4439787c77e70b60ef879e13af9bfcb688de834f
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 20:27:53 +0200

    allowed blog.ablaze.one as an image source

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'blog.ablaze.one'
       }
     ]
   },
