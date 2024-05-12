5c92d29db48f649c50903bb435bdffcee95470f4
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-12 17:16:56 +0200

    more shadcn

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,11 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import {Slot} from "@radix-ui/react-slot"
+import {cva, type VariantProps} from "class-variance-authority"
 
-import { cn } from "@/lib/utils"
+import {cn} from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -18,17 +18,40 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        rLink: "text-primary underline-offset-4 underline hover:no-underline",
+      },
+      align: {
+        default: "justify-center",
+        start: "justify-start",
+        center: "justify-center",
+        end: "justify-end",
+      },
+      paddingV: {
+        default: "py-2",
+        sm: "py-1",
+        lg: "py-3",
+        none: "py-0",
+      },
+      paddingH: {
+        default: "px-4",
+        sm: "px-3",
+        lg: "px-8",
+        none: "px-0",
       },
       size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        default: "h-10",
+        sm: "h-9 rounded-md",
+        lg: "h-11 rounded-md",
         icon: "h-10 w-10",
+        none: "",
       },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      align: "default",
+      paddingV: "default",
+      paddingH: "default",
     },
   }
 )
@@ -40,11 +63,11 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({className, variant, align, paddingV, paddingH, size, asChild = false, ...props}, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({paddingV, align, paddingH, variant, size, className}))}
         ref={ref}
         {...props}
       />
@@ -53,4 +76,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+export {Button, buttonVariants}
