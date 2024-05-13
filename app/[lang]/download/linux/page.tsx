f3cb5c92ea9718ac7062b32e7ef3dc544e34320c
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-13 23:59:53 +0200

    added platform pages

---
+++ app/[lang]/download/linux/page.tsx
@@ -0,0 +1,13 @@
+
+export default function LinuxDownload() {
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">Linux</h1>
+        <p>Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.</p>
+        <p>Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.</p>
+        <p>Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds. Linux is typically packaged in a Linux distribution.</p>
+      </div>
+    </div>
+  )
+}
---
+++ app/[lang]/download/macos/page.tsx
@@ -0,0 +1,11 @@
+
+export default function MacOSDownload() {
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">MacOS</h1>
+<p>macOS is a series of proprietary graphical operating systems developed and marketed by Apple Inc. since 2001. It is the primary operating system for Apple's Mac computers. Within the market of desktop, laptop and home computers, and by web usage, it is the second most widely used desktop OS, after Microsoft Windows.</p>
+      </div>
+    </div>
+  )
+}
