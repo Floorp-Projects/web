1d03f2def7364b788e355d5b3c43b97704e11485
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-13 09:51:12 +0200

    theme change and new icons

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import {Moon, Sun} from "lucide-react"
+import {RiSunFill, RiMoonLine} from "@remixicon/react";
 import {useTheme} from "next-themes"
 
 import {Button} from "@/components/ui/button"
@@ -12,16 +12,30 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
+function AbsoluteDiv({children}: { children: React.ReactNode }) {
+  return (
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden h-5 w-5">
+      {children}
+    </div>
+  )
+}
+
 export function ThemeSwitch() {
   const {setTheme} = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-          <Moon
-            className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+        <Button variant="outline" paddingH={"sm"} className="relative">
+          <div className="h-5 w-5"></div>
+          <AbsoluteDiv>
+            <RiSunFill
+              className="h-5 w-5 scale-100 dark:scale-50 rotate-0 transition-all dark:-rotate-90 hover:rotate-6 translate-y-0 dark:-translate-y-5 duration-1000"/>
+          </AbsoluteDiv>
+          <AbsoluteDiv>
+            <RiMoonLine
+              className="h-5 w-5 translate-y-5 transition-all duration-1000 dark:rotate-0 dark:translate-y-0"/>
+          </AbsoluteDiv>
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
