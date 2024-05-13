bee28fbfb06d42b245900dc7af9477a60a108fe5
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-13 23:59:53 +0200

    added platform pages

---
+++ app/[lang]/download/windows32/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Windows32() {
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-4xl font-bold">Windows 32-bit</h1>
+
+      </div>
+    </div>
+  )
+}
