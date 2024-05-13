1b8cccc963ac99c6b9b83915413f34b0c9aa0059
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-13 09:51:31 +0200

    started adding download page

--- app/[lang]/download/page.tsx
+++ app/[lang]/download/page.tsx
@@ -1,13 +1,25 @@
-import { Locale } from "@/i18n/i18n.config";
+import { getDictionary } from "@/i18n/dictionaries";
+import {Locale} from "@/i18n/i18n.config";
+import InfoAlert from "@/components/layout/info-alert";
 
 type DownloadProps = {
   params: { lang: Locale }
 };
 
-export default function Download({params: {lang}}: DownloadProps) {
+export default async function Download({params: {lang}}: DownloadProps) {
+  const dict = await getDictionary(lang);
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Download Page</h1>
-    </div>
+    <main className='w-full py-24'>
+      <div className="flex min-h-screen w-full flex-col items-center">
+        <div className="flex flex-col max-w-4xl">
+          <h1 className="text-4xl font-bold">{dict.downloadPage.download}</h1>
+          <p>{dict.downloadPage.description}</p>
+          <InfoAlert
+            title={dict.components.alerts.info}
+            description={dict.downloadPage.downloadAlert}
+          />
+        </div>
+      </div>
+    </main>
   );
 }
