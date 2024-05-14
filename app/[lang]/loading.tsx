246a9db75adf0bac9aef57debc2a455f45996a41
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-14 11:40:13 +0200

    respnsivity refactor

--- app/[lang]/loading.tsx
+++ app/[lang]/loading.tsx
@@ -1,10 +1,8 @@
-
-export default function Loading() {
+export default async function Loading() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center gap-4">
         <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-gray-100"></div>
-        <h1 className={'text-2xl font-bold'}>読み込み中</h1>
       </div>
     </div>
   )
