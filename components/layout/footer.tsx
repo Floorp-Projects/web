e731a74083690e34b7763ed534004a64528655bf
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-14 11:40:13 +0200

    respnsivity refactor

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -9,8 +9,8 @@ import LogoDark from "@/public/logo-dark.png";
 
 export default function Footer() {
   return (
-    <footer className="bottom-0 flex h-[20vh] w-full items-center flex-col gap-4 border-t bg-background">
-      <div className="flex w-full pt-4 max-w-4xl gap-4 justify-between items-center">
+    <footer className="bottom-0 flex h-48 w-full items-center justify-between flex-col p-6 gap-4 border-t bg-background">
+      <div className="flex w-full max-w-4xl gap-4 justify-between items-center">
         <div className={'flex gap-4 items-start justify-end flex-col'}>
           <ThemedImage
             darkImage={LogoDark}
@@ -18,17 +18,6 @@ export default function Footer() {
             alt={"logo"}
             rest={{width: 100, height: 50}}
           />
-          <div className={'flex gap-2 flex-row'}>
-            <Link href="https://aka.ablaze.one/discord" className={buttonVariants({variant: 'outline'})}>
-              <FaDiscord />
-            </Link>
-            <Link href="https://twitter.com/Floorp_Browser" className={buttonVariants({variant: 'outline'})}>
-              <FaTwitter />
-            </Link>
-            <Link href="https://github.com/floorp-Projects/floorp/" className={buttonVariants({variant: 'outline'})}>
-              <FaGithub />
-            </Link>
-          </div>
         </div>
         <p className={'text-center text-sm text-neutral-600 dark:text-gray-50'}>
           &copy;{new Date().getFullYear()} Floorp Browser
