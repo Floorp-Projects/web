8fa0c98c0c59decf75e678093296451e36d156d2
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-14 17:43:26 +0200

    dev badge layout and github release query progression

--- components/layout/landing/preview-parallax.tsx
+++ components/layout/landing/preview-parallax.tsx
@@ -2,14 +2,7 @@ import React from "react";
 import {HeroParallax, ParallaxProduct} from "@/components/ui/hero-parallax";
 import {Locale} from "@/i18n/i18n.config";
 import {getDictionary} from "@/i18n/dictionaries";
-import Dummy1 from "@/public/hero/dummy1.png";
-import Dummy2 from "@/public/hero/dummy2.png";
-import Dummy3 from "@/public/hero/dummy3.png";
-import Dummy4 from "@/public/hero/containers.png";
-import Dummy5 from "@/public/hero/floorp1.png";
-import Dummy6 from "@/public/hero/floorp2.png";
-import Dummy7 from "@/public/hero/genbun.png";
-import Dummy8 from "@/public/hero/ablaze-profile.png";
+import Dummy1 from "@/public/hero/floorp1.png";
 
 type LocaleProduct = {
   title: string;
@@ -26,30 +19,9 @@ type ImageProduct = {
 }
 
 const images = {
-  moonbeam: {
+  floorp: {
     image: Dummy1,
   },
-  cursor: {
-    image: Dummy2,
-  },
-  rogue: {
-    image: Dummy3,
-  },
-  editorially: {
-    image: Dummy4,
-  },
-  editrix: {
-    image: Dummy5,
-  },
-  genbun: {
-    image: Dummy6,
-  },
-  ablaze: {
-    image: Dummy7,
-  },
-  ablazeProfile: {
-    image: Dummy8,
-  }
 } as Record<string, ImageProduct>;
 
 const getProducts = (localeProducts: LocaleProduct[]): ParallaxProduct[] => {
