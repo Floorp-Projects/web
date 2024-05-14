0c6a4536238ccef749bbb9b0df85ce297ca9efcf
Joshua Hegedus <josh.hegedus@outlook.com>
2024-05-14 11:39:43 +0200

    made translation load dynamic

--- i18n/dictionaries.ts
+++ i18n/dictionaries.ts
@@ -1,13 +1,23 @@
 import 'server-only'
-
+import fs from 'fs'
+import path from 'path'
 import type { Locale } from './i18n.config';
 
-const dictionaries = {
-  en: () => import('@/dictionaries/en.json').then((module) => module.default),
-  ja: () => import('@/dictionaries/ja.json').then((module) => module.default),
-  hu: () => import('@/dictionaries/hu.json').then((module) => module.default),
-  ru: () => import('@/dictionaries/ru.json').then((module) => module.default),
-}
+const dictionariesPath = path.join(process.cwd(), 'i18n', 'dictionaries');
+const dictionaryFiles = fs.readdirSync(dictionariesPath)
+
+const enDict = import('@/dictionaries/en.json').then((module) => module.default);
+export type Dictionary = typeof enDict extends Promise<infer T> ? T : never;
+
+const dictionaries = Object.fromEntries(
+  dictionaryFiles.map((file) => {
+    const language = file.split('.')[0]
+    return [language, async () => {
+      const dictionary = await import(`./dictionaries/${file}`)
+      return dictionary.default
+    }]
+  })
+) as Record<Locale, () => Promise<Dictionary>>;
 
 export const getDictionary = async (locale: Locale) =>
   dictionaries[locale]?.() ?? await dictionaries.en();
