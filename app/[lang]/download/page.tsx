9723bd1148b3af419e9680d240ee55c1403396f4
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-15 14:21:56 +0200

    download page responsiveness

--- app/[lang]/download/page.tsx
+++ app/[lang]/download/page.tsx
@@ -37,34 +37,58 @@ export default async function DownloadPage({params: {lang}, searchParams}: Downl
     }
 
     return (
-      <div className={'mt-8'}>
-        <div className={'flex items-center gap-4 ml-4 mb-2'}>
-          <div className={'flex flex-col sm:flex-row gap-2 justify-center sm:justify-between w-full'}>
-            <p className={'text-sm text-muted-foreground text-center sm:text-left'}>
-              {f(dict.downloadPage.releaseDate, {date})}
-            </p>
-            <Separator orientation="vertical" className={'h-5 hidden sm:block'}/>
-            <Link
-              href={`https://github.com/Floorp-Projects/Floorp/releases/tag/${release.version}`}
-              target={'_blank'}
-              className={cn(
-                buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
-              )}
-            >
-              {release.version}
-            </Link>
-            <Separator orientation="vertical" className={'h-5 hidden sm:block'}/>
-            <Link
-              href={release.hashes}
-              className={cn(
-                buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
-              )}
-            >
-              {dict.downloadPage.downloadHashes}
-            </Link>
+      <div className={'flex flex-col sm:flex-row mt-8 gap-4 justify-between w-full'}>
+        <div>
+          <div className={'flex items-center gap-4 ml-4 mb-2'}>
+            <div className={'flex flex-col sm:flex-row gap-2 justify-center sm:justify-between w-full'}>
+              <p className={'text-sm text-muted-foreground text-center sm:text-left'}>
+                {f(dict.downloadPage.releaseDate, {date})}
+              </p>
+              <Separator orientation="vertical" className={'h-5 hidden sm:block'}/>
+              <Link
+                href={`https://github.com/Floorp-Projects/Floorp/releases/tag/${release.version}`}
+                target={'_blank'}
+                className={cn(
+                  buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
+                )}
+              >
+                {release.version}
+              </Link>
+              <Separator orientation="vertical" className={'h-5 hidden sm:block'}/>
+              <Link
+                href={release.hashes}
+                className={cn(
+                  buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
+                )}
+              >
+                {dict.downloadPage.downloadHashes}
+              </Link>
+            </div>
           </div>
+          <AssetsTable items={items} locale={dict.downloadPage.assetsTable}/>
         </div>
-        <AssetsTable items={items} locale={dict.downloadPage.assetsTable}/>
+        <ScrollArea className="h-80 w-36 rounded-md relative pt-10 border self-center">
+          <div className="p-4">
+            <h4 className="mb-4 text-sm font-medium absolute top-4 leading-none">{dict.downloadPage.versions}</h4>
+            <Separator className="absolute top-10 left-0"/>
+            {tags.map((tag, i) => (
+              <>
+                <div key={tag} className="text-sm">
+                  <Link
+                    href={`https://github.com/Floorp-Projects/Floorp/releases/tag/${tag}`}
+                    target={'_blank'}
+                    className={cn(
+                      buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
+                    )}
+                  >
+                    {tag}{i === 0 ? " - " + dict.downloadPage.latest : ''}
+                  </Link>
+                </div>
+                <Separator className="my-2"/>
+              </>
+            ))}
+          </div>
+        </ScrollArea>
       </div>
     )
   }
@@ -92,37 +116,14 @@ export default async function DownloadPage({params: {lang}, searchParams}: Downl
   }
 
   return (
-    <div className="flex flex-col sm:flex-row items-center sm:items-start gap-4">
+    <div className="flex flex-col w-full sm:flex-row items-center justify-between sm:items-start gap-4">
       <PlatformSelect
         locale={{...dict.downloadPage.dropdownLocale, detect: dict.downloadPage.dropdownLocale.detect}}
         checkbox={dict.downloadPage.daylight}
-        className={'mt-4'}
         alert={isDaylight ?
           <FAlert lang={lang} description={dict.downloadPage.daylight.alert} severity={'warning'}/> : null}
         platforms={getPlatformOptions()}
       />
-      <ScrollArea className="h-72 w-48 rounded-md relative pt-10 border">
-        <div className="p-4">
-          <h4 className="mb-4 text-sm font-medium absolute top-4 leading-none">{dict.downloadPage.versions}</h4>
-          <Separator className="absolute top-10 left-0"/>
-          {tags.map((tag, i) => (
-            <>
-              <div key={tag} className="text-sm">
-                <Link
-                  href={`https://github.com/Floorp-Projects/Floorp/releases/tag/${tag}`}
-                  target={'_blank'}
-                  className={cn(
-                    buttonVariants({variant: 'link', paddingV: 'none', paddingH: 'none', size: 'auto'}),
-                  )}
-                >
-                  {tag}{i === 0 ? " - " + dict.downloadPage.latest : ''}
-                </Link>
-              </div>
-              <Separator className="my-2"/>
-            </>
-          ))}
-        </div>
-      </ScrollArea>
     </div>
   )
 }
