ce57360d7ef1bf55c8560e5903f0c9a0790c875b
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-15 14:48:19 +0200

    added proper submodule

--- i18n/dictionaries.ts
+++ i18n/dictionaries.ts
@@ -3,17 +3,18 @@ import fs from 'fs'
 import path from 'path'
 import type { Locale } from './i18n.config';
 
-const dictionariesPath = path.join(process.cwd(), 'i18n', 'dictionaries');
-const dictionaryFiles = fs.readdirSync(dictionariesPath)
+const dictionariesPath = path.join(process.cwd(), 'i18n', 'dictionaries', 'i18n')
+const dictionaryDirectories = fs.readdirSync(dictionariesPath)
+const dictionaryFiles = dictionaryDirectories.flatMap((dir) => fs.readdirSync(path.join(dictionariesPath, dir)))
 
-const enDict = import('@/dictionaries/en.json').then((module) => module.default);
+const enDict = import('@/dictionaries/i18n/ja/ja.json').then((module) => module.default);
 export type Dictionary = typeof enDict extends Promise<infer T> ? T : never;
 
 const dictionaries = Object.fromEntries(
-  dictionaryFiles.map((file) => {
+  dictionaryFiles.filter((file) => file.endsWith('.json')).map((file) => {
     const language = file.split('.')[0]
     return [language, async () => {
-      const dictionary = await import(`./dictionaries/${file}`)
+      const dictionary = await import(`@/dictionaries/i18n/${language}/${language}.json`)
       return dictionary.default
     }]
   })
