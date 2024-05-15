b7fae42298fa44407bb6a126f7d09ed4ac63878a
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-15 14:01:36 +0200

    responsivity fixes

--- components/layout/hero-card.tsx
+++ components/layout/hero-card.tsx
@@ -15,7 +15,7 @@ export default function MainHeroCard({header, description, sourceCode, download}
   return (
     <CardContainer className="inter-var">
       <CardBody
-        className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
