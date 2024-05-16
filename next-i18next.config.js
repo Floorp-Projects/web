2dde93a324db0f235c5fedbc3e31fa705c41752a
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-16 22:47:57 +0200

    removed last part

--- next-i18next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-i18next').UserConfig} */
-module.exports = {
-  debug: process.env.NODE_ENV === 'development',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ja', 'ru', 'ko'],
-  },
-};
