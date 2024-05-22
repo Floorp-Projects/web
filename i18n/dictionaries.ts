b818f7b0ccf40ff1288acb9f9847cc9474bec307
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-23 00:00:39 +0200

    updated submodule and added new usage

--- i18n/dictionaries.ts
+++ i18n/dictionaries.ts
@@ -7,14 +7,14 @@ const dictionariesPath = path.join(process.cwd(), 'i18n', 'dictionaries', 'i18n'
 const dictionaryDirectories = fs.readdirSync(dictionariesPath)
 const dictionaryFiles = dictionaryDirectories.flatMap((dir) => fs.readdirSync(path.join(dictionariesPath, dir)))
 
-const enDict = import('@/dictionaries/i18n/ja/ja.json').then((module) => module.default);
+const enDict = import('@/dictionaries/i18n/en/dictionary.json').then((module) => module.default);
 export type Dictionary = typeof enDict extends Promise<infer T> ? T : never;
 
 const dictionaries = Object.fromEntries(
   dictionaryFiles.filter((file) => file.endsWith('.json')).map((file) => {
     const language = file.split('.')[0]
     return [language, async () => {
-      const dictionary = await import(`@/dictionaries/i18n/${language}/${language}.json`)
+      const dictionary = await import(`@/dictionaries/i18n/${language}/dictionary.json`)
       return dictionary.default
     }]
   })
