c1a4b572d737785f9e11225ec526fcd1e334a664
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-05-22 21:05:08 +0200

    card and hero and button fix

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,11 +13,11 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-popover hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "text-primary dark:text-primary-highlight underline-offset-4 underline hover:no-underline",
         rLink: "text-primary underline-offset-4 underline hover:no-underline",
       },
       align: {
