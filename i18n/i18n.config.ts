278bf7eece6da3cfe5bca26ace280e45bcece6dd
regularentropy <roottotalhax@gmail.com>
2024-10-12 00:16:25 +0200

    Merge branch 'dev'

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,7 +1,7 @@
 export const defaultLocale = "en"
-export const locales = ["en", "ja", "ko", "ru", "uk", "fr", "hu"];
+export const locales = ["en", "ja", "ru", "uk", "fr", "hu", "da"] as const;
 
-export const reviewedTranslations: string[] = ["en", "ja", "ko", "fr", "ru"]
+export const reviewedTranslations: string[] = ["en", "ja", "fr", "ru", "da"]
 export const inReviewTranslations: string[] = ["uk", "hu"]
 export const notStartedTranslations: string[] = []
 export const isReviewed = (locale: string) => reviewedTranslations.includes(locale);
@@ -11,10 +11,11 @@ export const isNotStarted = (locale: string) => notStartedTranslations.includes(
 export const langDict = {
   en: "English",
   ja: "日本語",
-  ko: "한국어",
+  // ko: "한국어", TODO: Had to be removed due to low quality translations
   ru: "Русский",
   hu: "Magyar",
   fr: "Français",
+  da: "Dansk",
   uk: "Українська",
 } as Record<string, string>;
 
