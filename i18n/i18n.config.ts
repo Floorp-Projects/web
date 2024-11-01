457ce11f139c12332d0d810095ae3b52cb5bbeff
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-01 17:06:55 +0100

    updated packages and started fixing languages

--- i18n/i18n.config.ts
+++ i18n/i18n.config.ts
@@ -1,16 +1,31 @@
-export const defaultLocale = "en"
-export const locales = ["en", "ja", "ru", "uk", "fr", "hu", "da"] as const;
+export const defaultLocale = "en";
+export const locales = [
+  "en",
+  "ja",
+  "zh-CN",
+  "zh-TW",
+  "ru",
+  "uk",
+  "fr",
+  "hu",
+  "da",
+] as const;
 
-export const reviewedTranslations: string[] = ["en", "ja", "fr", "ru", "da"]
-export const inReviewTranslations: string[] = ["uk", "hu"]
-export const notStartedTranslations: string[] = []
-export const isReviewed = (locale: string) => reviewedTranslations.includes(locale);
-export const isInReview = (locale: string) => inReviewTranslations.includes(locale);
-export const isNotStarted = (locale: string) => notStartedTranslations.includes(locale);
+export const reviewedTranslations: string[] = ["en", "ja", "fr", "ru", "da"];
+export const inReviewTranslations: string[] = ["uk", "hu"];
+export const notStartedTranslations: string[] = [];
+export const isReviewed = (locale: string) =>
+  reviewedTranslations.includes(locale);
+export const isInReview = (locale: string) =>
+  inReviewTranslations.includes(locale);
+export const isNotStarted = (locale: string) =>
+  notStartedTranslations.includes(locale);
 
 export const langDict = {
   en: "English",
   ja: "日本語",
+  "zh-CN": "简体中文",
+  "zh-TW": "正體中文",
   // ko: "한국어", TODO: Had to be removed due to low quality translations
   ru: "Русский",
   hu: "Magyar",
@@ -22,6 +37,6 @@ export const langDict = {
 export const i18n = {
   defaultLocale,
   locales: locales,
-} as const
+} as const;
 
-export type Locale = (typeof i18n)['locales'][number]
+export type Locale = (typeof i18n)["locales"][number];
