7ef599157c7756c5df38a62f0d2038ff5f58ad08
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-01 17:06:55 +0100

    updated packages and started fixing languages

--- i18n/dictionaries.ts
+++ i18n/dictionaries.ts
@@ -1,29 +1,40 @@
-import 'server-only'
-import type {Locale} from './i18n.config';
+import "server-only";
+import type { Locale } from "./i18n.config";
 
 const dictionaryFiles = [
-  'en.json',
-  'ja.json',
-  'ko.json',
-  'ru.json',
-  'hu.json',
-  'fr.json',
-  'uk.json',
-  'da.json',
+  "en.json",
+  "ja.json",
+  "ko.json",
+  "zh-CN",
+  "zh-TW",
+  "ru.json",
+  "hu.json",
+  "fr.json",
+  "uk.json",
+  "da.json",
 ];
 
-const enDict = import('@/dictionaries/i18n/en/dictionary.json').then((module) => module.default);
+const enDict = import("@/dictionaries/i18n/en/dictionary.json").then(
+  (module) => module.default,
+);
 export type Dictionary = typeof enDict extends Promise<infer T> ? T : never;
 
 const dictionaries = Object.fromEntries(
-  dictionaryFiles.filter((file) => file.endsWith('.json')).map((file) => {
-    const language = file.split('.')[0]
-    return [language, async () => {
-      const dictionary = await import(`@/dictionaries/i18n/${language}/dictionary.json`)
-      return dictionary.default
-    }]
-  })
+  dictionaryFiles
+    .filter((file) => file.endsWith(".json"))
+    .map((file) => {
+      const language = file.split(".")[0];
+      return [
+        language,
+        async () => {
+          const dictionary = await import(
+            `@/dictionaries/i18n/${language}/dictionary.json`
+          );
+          return dictionary.default;
+        },
+      ];
+    }),
 ) as Record<Locale, () => Promise<Dictionary>>;
 
 export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale]?.() ?? await dictionaries.en();
+  dictionaries[locale]?.() ?? (await dictionaries.en());
