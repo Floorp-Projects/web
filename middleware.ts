11d02ec2e142bfc7e9b61a56dfe2aecf5f8a98c0
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2024-11-11 14:49:34 +0100

    working middleware

--- middleware.ts
+++ middleware.ts
@@ -15,22 +15,18 @@ function combinePath(request: NextRequest, path: string): boolean {
   return request.nextUrl.pathname.startsWith(`/${getLocale(request)}/${path}`);
 }
 
-function getLocale(request: NextRequest): string | undefined {
-  try {
-    const negotiatorHeaders: Record<string, string> = {};
-    request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+function getLocale(request: NextRequest): string {
+  const negotiatorHeaders: Record<string, string> = {};
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
 
-    // @ts-ignore locales are readonly
-    const locales: string[] = i18n.locales;
-    let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-      locales,
-    );
-    return matchLocale(languages, locales, i18n.defaultLocale);
-  } catch (e) {
-    console.log("getLocale failed");
-    console.error(e);
-    return undefined;
-  }
+  const locales: string[] = i18n.locales.map((locale) =>
+    locale?.replace("_", "-"),
+  );
+  let languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales,
+  );
+  console.log("Languages: ", languages);
+  return matchLocale(languages, locales, i18n.defaultLocale);
 }
 
 const excludedPaths = [
@@ -47,10 +43,12 @@ const excludedPaths = [
 export function middleware(request: NextRequest) {
   let { pathname } = request.nextUrl;
   try {
-    const pathnameHasLocale = locales.some(
-      (locale) =>
-        pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
-    );
+    const pathnameHasLocale = locales
+      .map((locale) => locale.replace("_", "-"))
+      .some(
+        (locale) =>
+          pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`,
+      );
 
     // Check if any of the excluded paths are in the request
     if (excludedPaths.some((path) => pathname.includes(path))) {
@@ -58,12 +56,6 @@ export function middleware(request: NextRequest) {
     }
 
     let locale = getLocale(request);
-    if (locale === undefined) {
-      console.log("Could not determine locale, redirecting to en");
-      locale = defaultLocale;
-    } else {
-      console.log(`Detected locale: ${locale}`);
-    }
     if (pathnameHasLocale) {
       const selectedLocale = pathname.split("/")[1];
       if (isNotStarted(selectedLocale || "")) {
