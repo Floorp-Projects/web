4e3333c342c6cd3374eaa577c7450fed2cb2c2db
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-01-12 22:30:29 +0100

    feat: added code block and copy buddon component

---
+++ components/code/code-block.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React, {ReactNode} from "react";
+import CopyButton from "@/components/code/copy2clipboard";
+
+export default function CodeBlock({children, copyString}: { children: ReactNode, copyString?: string }) {
+
+  return (
+    <div className={'relative'}>
+      <CopyButton content={copyString || children as string} className={'absolute top-0 right-0'}/>
+      <pre className={'bg-gray-300 text-black p-4 pt-8 rounded-md'}>
+        {children}
+      </pre>
+    </div>
+  );
+}
