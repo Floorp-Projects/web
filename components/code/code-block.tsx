ea13e9e20fdb22fd8fdb7018a2ea3e4ddfbbd307
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-01-18 19:52:23 +0100

    feat: dark theme support for codeblock

--- components/code/code-block.tsx
+++ components/code/code-block.tsx
@@ -1,15 +1,20 @@
 'use client';
 import React, {ReactNode} from "react";
 import CopyButton from "@/components/code/copy2clipboard";
+import {cn} from "@/lib/utils";
 
-export default function CodeBlock({children, copyString}: { children: ReactNode, copyString?: string }) {
+export default function CodeBlock({children, className, copyString}: {
+  children: ReactNode,
+  className?: string,
+  copyString?: string
+}) {
 
   return (
-    <div className={'relative'}>
-      <CopyButton content={copyString || children as string} className={'absolute top-0 right-0'}/>
-      <pre className={'bg-gray-300 text-black p-4 pt-8 rounded-md overflow-x-auto'}>
+    <div className={cn('relative bg-gray-300 dark:bg-gray-700 rounded-md flex flex-row p-4 w-full justify-between', className)}>
+      <pre className={'text-foreground overflow-x-auto content-center'}>
         {children}
       </pre>
+      <CopyButton content={copyString || children as string} className={''}/>
     </div>
   );
 }
