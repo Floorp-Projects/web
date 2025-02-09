7923ff88699b648a8283a61887a73782a336ee14
ヘゲディス・ジョシュア <josh.hegedus@outlook.com>
2025-02-09 19:14:10 +0100

    feat: added `preClassName` to add custom styles to pre

--- components/code/code-block.tsx
+++ components/code/code-block.tsx
@@ -3,9 +3,10 @@ import React, {ReactNode} from "react";
 import CopyButton from "@/components/code/copy2clipboard";
 import {cn} from "@/lib/utils";
 
-export default function CodeBlock({children, className, copyString}: {
+export default function CodeBlock({children, className, copyString, preClassName}: {
   children: ReactNode,
   className?: string,
+  preClassName?: string,
   copyString?: string
 }) {
 
@@ -16,7 +17,7 @@ export default function CodeBlock({children, className, copyString}: {
         className
       )}
     >
-      <pre className={'text-foreground col-span-5 whitespace-pre-wrap w-full content-center overflow-x-auto'}>
+      <pre className={cn('text-foreground col-span-5 whitespace-pre-wrap w-full content-center overflow-x-auto', preClassName)}>
         {children}
       </pre>
       <CopyButton content={copyString || children as string} className={'m-0 self-center'}/>
