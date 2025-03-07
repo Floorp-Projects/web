4eee82aa5fdf75cca2051da9db5f908805d2f21b
typeling1578 <pub@typeling1578.dev>
2025-03-07 14:38:30 +0900

    fix: loading style

--- app/[lang]/loading.tsx
+++ app/[lang]/loading.tsx
@@ -2,7 +2,7 @@ export default async function Loading() {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center gap-4">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-gray-100"></div>
+        <div className="animate-spin rounded-full h-32 w-32 border-4 border-l-gray-900 dark:border-l-gray-100"></div>
       </div>
     </div>
   )
